52478df3e4b2a1d0c97b888cd20b110733ace48a
shivakshiras-coder <shivakshi.ras@gmail.com>
2026-08-16 17:36:26 +0100

    more changes

--- my-first-terraform/main.tf
+++ my-first-terraform/main.tf
@@ -9,8 +9,17 @@ terraform {
 
 provider "aws" {
   region = "eu-west-2"  # London — pick whatever region is close to you
+  profile = "devwork"
 }
 
 resource "aws_s3_bucket" "my_first_bucket" {
-  bucket = "my-first-terraform-bucket-yourname-12345"
+  bucket = "my-first-terraform-bucket-aadya-12345"
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.my_first_bucket.bucket
+}
+
+output "bucket_arn" {
+  value = aws_s3_bucket.my_first_bucket.arn
 }
